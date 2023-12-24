aad0ecc82f9e67ece4c105899f40cb8cf295f7cc
ralph yozzo <ralph@brooklynmarathon.com>
2023-12-24 12:50:15 -0500

    ralph-desktop

--- examples/client/node.js/storeDirectory.js
+++ examples/client/node.js/storeDirectory.js
@@ -2,7 +2,8 @@ import fs from 'fs'
 import { NFTStorage, File } from 'nft.storage'
 
 const endpoint = 'https://api.nft.storage' // the default
-const token = 'API_KEY' // your API key from https://nft.storage/manage
+// const token = 'API_KEY' // your API key from https://nft.storage/manage
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDliODc4YzZEMEQ1NDU4MDBjQzMzRjA2NTRCZGNFYTU5QjY3MURmOUMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTcwMzQzOTcyMzI2NCwibmFtZSI6InVwbG9hZDEifQ.OeT2fdEaU4s3XV_v0IM1bJcX-HqDs20J3fDz8ipahhw' // your API key from https://nft.storage/manage
 
 async function main() {
   const storage = new NFTStorage({ endpoint, token })
